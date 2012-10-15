9549c50ed81dd3cb0c6f1e372ec008c568f15e7f
Stefan Vizzari <svizzari@zendesk.com>
2012-10-16 08:56:57 +1100

    Removed dummy data, began to make requests to Magento api

--- app.js
+++ app.js
@@ -7,85 +7,82 @@
 		defaultState: 'loading',
 
 		profileData: {
-			id: 3424,
-			name: 'Jennifer Hansen',
-			profile_uri: 'http://jens-shop.com/index.php/admin/system_account',
-			orders_uri: 'http://jens-shop.com/index.php/admin/sales_order',
-			recent_orders: [
-				{
-					order_id: 576899,
-					status: 'Pending PayPal',
-					status_code: 'pending',
-					order_uri: 'http://jens-shop.com/index.php/admin/sales_order/576899',
-					titles: 'Order 1 - Awesome!'
-				},
-				{
-					order_id: 343267,
-					status: 'Shipped',
-					status_code: 'shipped',
-					order_uri: 'http://jens-shop.com/index.php/admin/sales_order/343267',
-					titles: 'Order 2 - Super!'
-				},
-				{
-					order_id: 986642,
-					status: 'Cancelled',
-					status_code: 'cancelled',
-					order_uri: 'http://jens-shop.com/index.php/admin/sales_order/986642',
-					titles: 'Order 3 - Still OK!'
-				}
-			],
-			total_orders: 2,
-			phone: '+61 3 9123 4567',
-			group: 'Administrators',
-			member_since: 'May 24, 2012, 11:46:33AM (EST)'
+			name           : 'John Citizen',
+			recent_orders  : [],
+			customer_since : new Date()
 		},
 
 		resources: {
-			MAGENTO_PROFILE_URI: '%@/zendesk/api/profile.php'
+			MAGENTO_PROFILE_URI : '%@/zendesk/api/customers/%@',
+			ORDER_URI           : '%@/zendesk/api/orders/%@'
 		},
 
 		requests: {
-			'getProfile': function() { return this._getRequest(helpers.fmt(this.resources.MAGENTO_PROFILE_URI, this.settings.url)); }
+			'getProfile'      : function(email) { return this._getRequest(helpers.fmt(this.resources.MAGENTO_PROFILE_URI, this.settings.url, email)); },
+			'getRecentOrders' : function() { return this._getRequest(helpers.fmt(this.resources.MAGENTO_PROFILE_URI)); },
+			'getOrder'        : function(orderNumber) { return this._getRequest(helpers.fmt(this.resources.MAGENTO_PROFILE_URI, this.settings.url, orderNumber)); }
 		},
 
 		events: {
-			'app.activated': 'handleAppActivated',
-			'getProfile.done': 'handleGetProfile'
-		},
-
-		_ajax: function(request, expectedData) {
-			var app = this,
-				evts = this.events,
-				fn = null;
-			_.delay(function() {
-				var promises = ['done', 'always'];
-				_.each(promises, function(p) {
-					fn = evts[helpers.fmt('%@.%@', request, p)];
-					if (!_.isUndefined(fn))
-					{
-						app[fn].apply(app, [_.extend({fake: true}, expectedData || {})]);
-					}
-				});
-			}, 250);
+			'app.activated'        : 'handleAppActivated',
+			'getProfile.done'      : 'handleGetProfile',
+			'getRecentOrders.done' : 'handleGetRecentOrders',
+			'getOrder.done'        : 'handleGetOrder',
+			'getProfile.fail'      : 'handleFail',
+			'getOrder.fail'        : 'handleFail'
 		},
 
 		handleAppActivated: function() {
 			// App was activated.
-			// Let's load user profile from Magento.
-			this._ajax('getProfile', this.profileData);
+			var email;
+			try {
+				email = this.ticket().requester().email();
+			} catch (err) {
+				email = 'chris@chnorton.com.au';
+			}
+			this.ajax('getProfile', email);
 		},
 
 		handleGetProfile: function(data) {
+			if (_.has(data, 'error'))
+			{
+				this.showError(this.I18n.t('global.error.title'), this.I18n.t('profile.error'));
+				return;
+			}
+			// console.log('Profile data ---');
+			console.log(data);
+			// We'll do a little transformation on the data and store locally.
+			this.profileData = data;
+			// Add customer since value
+			var d = new Date(Date.parse(data.created));
+			this.profileData.customer_since = d.toLocaleString();
 			// Got the profile data, populate interface
-			this.switchTo('profile', data);
+			this.switchTo('profile', this.profileData);
+		},
+
+		handleFail: function() {
+			// Show fail message
+			this.showError();
+		},
+
+		handleClickBackBtn: function(evt) {
+			this.switchTo('profile', this.profileData);
+			alert('blah');
 		},
 
 		_getRequest: function(resource) {
 			return {
-				url: resource
-				//method: 'GET'
-				// Some additional params required?
+				url      : resource,
+				method   : 'GET',
+				dataType : 'json'
 			};
+		},
+
+		showError: function(title, msg) {
+			this.switchTo('error', {
+				title: title || this.I18n.t('global.error.title'),
+				message: msg || this.I18n.t('global.error.message')
+			});
 		}
 
 	};
